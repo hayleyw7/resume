fed7c35e6fd58f7cc6eebb4e1b1b39a00e11359f
Hayley Witherell <hayleywitherell@gmail.com>
2024-04-14 12:28:20 -0600

    Add media section

--- src/js/index.js
+++ src/js/index.js
@@ -1,53 +1,72 @@
+document.addEventListener("DOMContentLoaded", function() {
+  addClickListenerToListSection("projects");
+  addClickListenerToListSection("media");
+});
+
 function shouldShowRepo(repoName) {
   const hiddenKeywords = [
-    'practice', 'curriculum', 'prework', 'fundamentals', 'hayleyw7', 'homework', '2', 'first', 'freyr', 'api', 'resources', 'intro', 'pong', 'skills', 'assignments', 'markdown', 'workshop', 'resume', 'playground', 'practice', 'refactor', 'debug', 'eventPractice', 'vocalization', 'kit', 'test', 'dog', 'library', 'hello', 'Hello', 'demo', 'vanilla', 'airtable', "jq", "slim", 'bootstrap', 'best'
+    "practice", "curriculum", "prework", "fundamentals", "hayleyw7", "homework", "2", "first", "freyr", "api", "resources", "intro", "pong", "skills", "assignments", "markdown", "workshop", "resume", "playground", "practice", "refactor", "debug", "eventPractice", "vocalization", "kit", "test", "dog", "library", "hello", "Hello", "demo", "vanilla", "airtable", "jq", "slim", "bootstrap", "best"
   ];
 
   return !hiddenKeywords.some(keyword => repoName.includes(keyword));
-}
+};
 
 function capitalizeWord(word) {
   const lowerWord = word.toLowerCase();
   switch (lowerWord) {
-    case 'p':
-    case 'z':
-    case 's':
-    case 'starter':
-    case 'hello':
-    case 'terminal':
-    case 'oss':
-    case 'satans':
-    case 'code':
-    case 'confirmation':
-      return '';
-    case 'js':
-    case 'javascript' :
-      return 'JS';
-    case 'ts': return 'TypeScript';
-    case 'whats': return "What's";
-    case 'localstorage': return 'localStorage';
-    case 'romcom': return 'RomCom';
-    case 'anon': return 'Anonymous';
-    case 'vis': return 'Visualization';
-    case 'flutterdemo': return 'Flutter Demo';
-    case 'operational': return 'Op';
-    case 'computer': return 'PC';
-    case 'scrollbars': return 'Scroll';
-    case 'todo': return 'To Do';
-    case 'sandbox': return 'Capture The Flag';
-    case 'foundations': return 'Mythicals';
-    default: return word.charAt(0).toUpperCase() + word.slice(1);
-  }
-}
+    case "p":
+    case "z":
+    case "s":
+    case "starter":
+    case "hello":
+    case "terminal":
+    case "oss":
+    case "satans":
+    case "code":
+    case "confirmation":
+      return "";
+    case "js":
+    case "javascript":
+      return "JS";
+    case "ts":
+      return "TypeScript";
+    case "whats":
+      return "What's";
+    case "localstorage":
+      return "localStorage";
+    case "romcom":
+      return "RomCom";
+    case "anon":
+      return "Anonymous";
+    case "vis":
+      return "Visualization";
+    case "flutterdemo":
+      return "Flutter Demo";
+    case "operational":
+      return "Op";
+    case "computer":
+      return "PC";
+    case "scrollbars":
+      return "Scroll";
+    case "todo":
+      return "To Do";
+    case "sandbox":
+      return "Capture The Flag";
+    case "foundations":
+      return "Mythicals";
+    default:
+      return word.charAt(0).toUpperCase() + word.slice(1);
+  };
+};
 
 function formatRepoName(repoName) {
   const repoWords = repoName.split(/[-_]|(?=[A-Z])/);
-  return repoWords.map(capitalizeWord).join(' ');
-}
+  return repoWords.map(capitalizeWord).join(" ");
+};
 
 function displayProjects(repos) {
-  const projectSection = document.querySelector('.portfolio');
-  const projectList = projectSection.querySelector('ul');
+  const projectSection = document.querySelector(".projects");
+  const projectList = projectSection.querySelector("ul");
 
   repos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
 
@@ -67,60 +86,85 @@ function displayProjects(repos) {
 
     appendProjectToList(projectList, repoItem);
   });
-
-  addClickListenerToPortfolioHeading();
-}
+};
 
 function appendProjectToList(list, projectItem) {
-  const listItem = document.createElement('li');
-  const anchor = document.createElement('a');
+  const listItem = document.createElement("li");
+  const anchor = document.createElement("a");
   anchor.href = projectItem.url;
   anchor.innerText = projectItem.name;
-  const dateSpan = document.createElement('span');
+  const dateSpan = document.createElement("span");
   dateSpan.textContent = ` (${projectItem.date})`;
 
   listItem.appendChild(anchor);
   listItem.appendChild(dateSpan);
   list.appendChild(listItem);
-}
+};
 
-function addClickListenerToPortfolioHeading() {
-  const portfolioHeading = document.querySelector('.portfolio-heading');
-  const projectList = document.querySelector('.project-list');
+function addClickListenerToListSection(section) {
+  const sectionMapping = {
+    projects: {
+      heading: ".projects-heading",
+      list: ".projects-list",
+      otherList: ".media-list"
+    },
+    media: {
+      heading: ".media-heading",
+      list: ".media-list",
+      otherList: ".projects-list"
+    },
+  };
 
-  let isProjectListHidden = true;
+  const { heading, list, otherList } = sectionMapping[section];
 
-  portfolioHeading.addEventListener('click', function() {
-    isProjectListHidden = !isProjectListHidden;
+  const headingElement = document.querySelector(heading);
+  const listElement = document.querySelector(list);
+  const otherListElement = document.querySelector(otherList);
 
-    if (!isProjectListHidden) {
-      portfolioHeading.classList.add('expanded');
-      projectList.classList.remove('hidden');
+  headingElement.addEventListener("click", function() {
+    let isListHidden = listElement.classList.contains("hidden");
+
+    if (!otherListElement.classList.contains("hidden")) {
+      otherListElement.classList.add("hidden");
+      const otherHeading = document.querySelector(otherList.replace("-list", "-heading"));
+      if (otherHeading) {
+        otherHeading.classList.remove("expanded");
+        if (otherHeading.classList.contains("media-heading")) {
+          otherHeading.style.marginTop = "";
+        };
+      };
+    };
+
+    if (isListHidden) {
+      listElement.classList.remove("hidden");
+      headingElement.classList.add("expanded");
+      if (heading === '.media-heading') {
+        headingElement.style.marginTop = "0";
+      };
     } else {
-      portfolioHeading.classList.remove('expanded');
-      projectList.classList.add('hidden');
-    }
+      listElement.classList.add("hidden");
+      headingElement.classList.remove("expanded");
+      if (heading === '.media-heading') {
+        headingElement.style.marginTop = "";
+      };
+    };
 
-    portfolioHeading.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
+    headingElement.scrollIntoView({
+      behavior: "smooth",
+      block: "start"
     });
   });
-}
+};
 
-document.addEventListener('DOMContentLoaded', function() {
-  addClickListenerToPortfolioHeading();
-});
-
-fetch('https://api.github.com/users/hayleyw7/repos?per_page=200')
+fetch("https://api.github.com/users/hayleyw7/repos?per_page=200")
   .then(response => {
     if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
+      throw new Error("Network response was not ok");
+    };
+        return response.json();
   })
   .then(displayProjects)
   .catch(error => {
-    console.error('Error fetching projects from GitHub:', error);
+    console.error("Error fetching projects from GitHub:", error);
     document.querySelector(".projects-section").style.display = "none";
   });
