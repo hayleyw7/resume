7f0429486a1be6471a222aa1b63982e520965b4b
Hayley Witherell <hayleywitherell@gmail.com>
2025-02-20 16:19:22 -0700

    Update header animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,19 +6,19 @@
 
 	// Breakpoints
 	breakpoints({
-		xlarge: [ '1141px',  '1680px' ],
-		large: [ '981px',   '1140px' ],
-		medium: [ '737px',   '980px'  ],
-		small: [ '481px',   '736px'  ],
-		xsmall: [ '321px',   '480px'  ],
-		xxsmall: [ null,      '320px'  ]
+		xlarge: [ '1141px', '1680px' ],
+		large: [ '981px', '1140px' ],
+		medium: [ '737px', '980px'  ],
+		small: [ '481px', '736px'  ],
+		xsmall: [ '321px', '480px'  ],
+		xxsmall: [ null, '320px'  ]
 	});
 
 	// Load animations
 	$window.on('load', function() {
 		window.setTimeout(function() {
 			$body.removeClass('is-preload');
-		}, 100);
+		}, 1);
 	});
 
 	// Scrolly
