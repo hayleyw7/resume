3bedae5e7c384bf9a72ce4b5b4e236ac4f2d4e11
Hayley Witherell <hayleywitherell@gmail.com>
2025-02-20 13:51:16 -0700

    Rm html classes not in css

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,12 +6,12 @@
 
 	// Breakpoints
 	breakpoints({
-		xlarge:   [ '1141px',  '1680px' ],
-		large:    [ '981px',   '1140px' ],
-		medium:   [ '737px',   '980px'  ],
-		small:    [ '481px',   '736px'  ],
-		xsmall:   [ '321px',   '480px'  ],
-		xxsmall:  [ null,      '320px'  ]
+		xlarge: [ '1141px',  '1680px' ],
+		large: [ '981px',   '1140px' ],
+		medium: [ '737px',   '980px'  ],
+		small: [ '481px',   '736px'  ],
+		xsmall: [ '321px',   '480px'  ],
+		xxsmall: [ null,      '320px'  ]
 	});
 
 	// Load animations
